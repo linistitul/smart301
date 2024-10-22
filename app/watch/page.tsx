44db354a60fceeaebb65ba02afbd8b931a808461
sebaptcd <dev@sebastian.ghiveci.com>
2024-10-22 15:38:21 +0200

    firsts dynamic meta experiment

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -1,14 +1,34 @@
 // app/watch/page.tsx
 
-import React from 'react';
+"use client"; // This directive makes the component a Client Component
 
-const AboutPage = () => {
+import React, { useEffect } from 'react';
+
+const Watch = () => {
+    useEffect(() => {
+        // Get the last part of the URL
+        const pathParts = window.location.pathname.split('/');
+        const lastPart = pathParts[pathParts.length - 1];
+
+        // Update meta tags
+        document.title = `Watch - ${lastPart}`;
+        const metaDescription = document.querySelector('meta[name="description"]');
+        if (metaDescription) {
+            metaDescription.setAttribute('content', `This page is for ${lastPart}.`);
+        } else {
+            const newMetaDescription = document.createElement('meta');
+            newMetaDescription.name = 'description';
+            newMetaDescription.content = `This page is for ${lastPart}.`;
+            document.head.appendChild(newMetaDescription);
+        }
+    }, []);
+
     return (
         <div>
-            <h1>About Us</h1>
-            <p>This is the about page.</p>
+            <h1>Hello!</h1>
+            <p>This is a page.</p>
         </div>
     );
 };
 
-export default AboutPage;
+export default Watch;
